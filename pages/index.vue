763ad5a0177c2b4db9520c56a86399393b8b6861
Hiroki Kawaguchi <kawakanu@gmail.com>
2021-07-02 16:32:51 +0900

    add: code to debug watch functions

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,48 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div>{{ r1 }}</div>
+    <div>{{ r2 }}</div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { defineComponent, ref, watch, watchEffect } from '@nuxtjs/composition-api'
 
-export default Vue.extend({})
+export default defineComponent({
+  setup() {
+    function fetch(): Promise<string> {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve("OK")
+        }, 1000)
+      })
+    }
+
+    const r1 = ref("--")
+    const r2 = ref("--")
+
+    fetch()
+      .then(res => {
+        r1.value = res
+        r2.value = res
+      })
+
+    // https://v3.ja.vuejs.org/guide/reactivity-computed-watchers.html#watch
+    watch(r1, r1 => {
+      // OK のみ表示される
+      console.log(`r1: ${r1}`)
+    })
+
+    // https://v3.ja.vuejs.org/guide/reactivity-computed-watchers.html#watcheffect
+    watchEffect(() => {
+      // -- と OK が表示される
+      console.log(`r2: ${r2.value}`)
+    })
+
+    return {
+      r1,
+      r2,
+    }
+  },
+})
 </script>
